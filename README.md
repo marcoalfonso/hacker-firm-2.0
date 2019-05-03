f59ffb50949c4e4dee2c5e811a3e745b571f8f25
Marco Lavielle <marcoalfonso@gmail.com>
2019-05-03 17:10:30 +1000

    feature - inital setup

---
+++ README.md
@@ -0,0 +1,27 @@
+# Hacker Firm Website Version 2.0
+Hacker Firm consultancy website.
+
+## Getting Started
+
+To get started, simply clone the repository and run `npm install`
+
+```
+# Clone the repo
+git clone https://github.com/marcoalfonso/hacker-firm-v2.git
+
+# Move into the new directory
+cd hacker-firm-v2/
+
+# Install npm packages
+npm install
+
+# Start up the next.js dev server, browse to http://localhost:3000/
+npm run dev
+```
+
+## Generating the Static Site
+
+```
+# Build and export the site, the static files are generated in the out/ folder
+npm run export
+```
---
+++ components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hacker Firm</h1>
+            </header>
+            <div className="content">
+                <p>Sydney digital agency<br />
+                Web, blockchain and VR development.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Learn More</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
